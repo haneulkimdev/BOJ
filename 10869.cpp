1c9fa397b4af4a7d82fac4a643dec81327b1c57e
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-06 20:23:19 +0900

    [BOJ] 10869번: 사칙연산

---
+++ 10869.cpp
@@ -0,0 +1,15 @@
+// http://boj.kr/43f50802ed524cc981aa0006f6f1a4f8
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << '\n';
+  cout << a - b << '\n';
+  cout << a * b << '\n';
+  cout << a / b << '\n';
+  cout << a % b;
+}
