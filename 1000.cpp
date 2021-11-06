546814e0d5ea5c58d2207871352fce364770a3a1
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-06 17:22:01 +0900

    [BOJ] 1000번: A+B

---
+++ 1000.cpp
@@ -0,0 +1,11 @@
+// http://boj.kr/0767e887c4424aa981f28a590bae0356
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+}
