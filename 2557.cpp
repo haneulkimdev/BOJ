ecdcc55d5729aaf03e43778b5eac1300c9b311c4
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-06 17:24:51 +0900

    [BOJ] 2557번: Hello World

---
+++ 2557.cpp
@@ -0,0 +1,9 @@
+// http://boj.kr/1d7afd6db2e2494fb1fd3f4014d81f4b
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << "Hello World!";
+}
