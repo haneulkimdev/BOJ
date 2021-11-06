6d7096c1cb7aecc5d447fe9500c4c96be8aa70c6
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-06 17:30:27 +0900

    [BOJ] 10171번: 고양이

---
+++ 10171.cpp
@@ -0,0 +1,12 @@
+// http://boj.kr/4e3e8c96446644438a975714697c36aa
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << "\\    /\\" << '\n';
+  cout << " )  ( \')" << '\n';
+  cout << "(  /  )" << '\n';
+  cout << " \\(__)|";
+}
