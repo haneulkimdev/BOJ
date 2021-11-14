ec1a3e9127a6c198bbcb9fab40515f3e7920a26e
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-14 22:45:58 +0900

    [BOJ] 15552번: 빠른 A+B

---
+++ 15552.cpp
@@ -0,0 +1,15 @@
+// http://boj.kr/d186726ee4c449daa7e0f99e02bb5184
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+  }
+}
