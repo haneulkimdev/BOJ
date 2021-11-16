28125fbf295a533e7c1a1e04f4e21a65b67aa18e
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-16 21:15:16 +0900

    [BOJ] 10808번: 알파벳 개수

---
+++ 10808.cpp
@@ -0,0 +1,13 @@
+// http://boj.kr/5659ef405d914e8cb164a847a0a6de5d
+#include <bits/stdc++.h>
+using namespace std;
+
+int freq[26];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  cin >> s;
+  for (auto c : s) freq[c - 'a']++;
+  for (int i = 0; i < 26; i++) cout << freq[i] << ' ';
+}
