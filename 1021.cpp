8dfcd796e075d98aedf41e29a1746346c6ae0352
Haneul Kim <haneul.kim.dev@gmail.com>
2021-11-28 16:55:15 +0900

    [BOJ] 1021번: 회전하는 큐

---
+++ 1021.cpp
@@ -0,0 +1,30 @@
+// http://boj.kr/cd34068678634afe97a82033d03545db
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  deque<int> DQ;
+  for (int i = 1; i <= n; i++) DQ.push_back(i);
+  int ans = 0;
+  while (m--) {
+    int t;
+    cin >> t;
+    int idx = find(DQ.begin(), DQ.end(), t) - DQ.begin();
+    while (DQ.front() != t) {
+      if (idx < (int)DQ.size() - idx) {
+        DQ.push_back(DQ.front());
+        DQ.pop_front();
+      } else {
+        DQ.push_front(DQ.back());
+        DQ.pop_back();
+      }
+      ans++;
+    }
+    DQ.pop_front();
+  }
+  cout << ans;
+}
