09aa097ece3d883bd8dda4b182142a35f7fb2e04
Haneul Kim <haneul.kim.dev@gmail.com>
2022-02-06 00:01:56 +0900

    [BOJ] 15666번: N과 M (12)

---
+++ 15666.cpp
@@ -0,0 +1,34 @@
+// http://boj.kr/99c67a91b04244d6af7822a614b6c908
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[8];
+int num[8];
+
+void func(int k) {
+  if (k == m) {
+    for (int i = 0; i < m; i++) cout << num[arr[i]] << ' ';
+    cout << '\n';
+    return;
+  }
+  int st = 0;
+  if (k != 0) st = arr[k - 1];
+  int tmp = 0;
+  for (int i = st; i < n; i++) {
+    if (tmp != num[i]) {
+      arr[k] = i;
+      tmp = num[i];
+      func(k + 1);
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) cin >> num[i];
+  sort(num, num + n);
+  func(0);
+}
