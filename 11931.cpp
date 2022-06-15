8fe750b734b2b4fca34c80996b354ab6624c6706
Haneul Kim <haneul.kim.dev@gmail.com>
2022-06-15 17:38:55 +0900

    [BOJ] 11931번: 수 정렬하기 4

---
+++ 11931.cpp
@@ -0,0 +1,41 @@
+// http://boj.kr/c3a04dfade5b4aff83ffe4fa7164a476
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[1000000];
+int tmp[1000000];
+
+void merge(int st, int en) {
+  int mid = (st + en) / 2;
+  int lidx = st;
+  int ridx = mid;
+  for (int i = st; i < en; i++) {
+    if (ridx == en)
+      tmp[i] = arr[lidx++];
+    else if (lidx == mid)
+      tmp[i] = arr[ridx++];
+    else if (arr[lidx] >= arr[ridx])
+      tmp[i] = arr[lidx++];
+    else
+      tmp[i] = arr[ridx++];
+  }
+  for (int i = st; i < en; i++) arr[i] = tmp[i];
+}
+
+void merge_sort(int st, int en) {
+  if (en - st == 1) return;
+  int mid = (st + en) / 2;
+  merge_sort(st, mid);
+  merge_sort(mid, en);
+  merge(st, en);
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  merge_sort(0, n);
+  for (int i = 0; i < n; i++) cout << arr[i] << '\n';
+}
