13c23e08e4e7398d844c97c7b453544871ee2a6a
Haneul Kim <haneul.kim.dev@gmail.com>
2022-06-18 13:45:02 +0900

    [BOJ] 11650번: 좌표 정렬하기

---
+++ 11650.cpp
@@ -0,0 +1,43 @@
+// http://boj.kr/e018f100a1cc4751bb16b22c853835c6
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+int n;
+pair<int, int> arr[100000];
+pair<int, int> tmp[100000];
+
+void merge(int st, int en) {
+  int mid = (st + en) / 2;
+  int lidx = st;
+  int ridx = mid;
+  for (int i = st; i < en; i++) {
+    if (ridx == en)
+      tmp[i] = arr[lidx++];
+    else if (lidx == mid)
+      tmp[i] = arr[ridx++];
+    else if (arr[lidx] <= arr[ridx])
+      tmp[i] = arr[lidx++];
+    else
+      tmp[i] = arr[ridx++];
+  }
+  for (int i = st; i < en; i++) arr[i] = tmp[i];
+}
+
+void merge_sort(int st, int en) {
+  if (en - st == 1) return;
+  int mid = (st + en) / 2;
+  merge_sort(st, mid);
+  merge_sort(mid, en);
+  merge(st, en);
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> arr[i].X >> arr[i].Y;
+  merge_sort(0, n);
+  for (int i = 0; i < n; i++) cout << arr[i].X << ' ' << arr[i].Y << '\n';
+}
