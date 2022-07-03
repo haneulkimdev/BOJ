7fd9a2c0778c166a10657f781005066e343662cd
Haneul Kim <haneul.kim.dev@gmail.com>
2022-07-03 15:32:27 +0900

    [BOJ] 1001번: A-B

---
+++ 1001.cpp
@@ -0,0 +1,11 @@
+// http://boj.kr/76bbe9a10300408f8e4cbb791f84b59d
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a - b;
+}
