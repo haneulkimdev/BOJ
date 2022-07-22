0a9d3134f2b661d8f716a481552baa2b5b4b5a1d
Haneul Kim <haneul.kim.dev@gmail.com>
2022-07-22 20:23:22 +0900

    [BOJ] 1008번: A/B

---
+++ 1008.cpp
@@ -0,0 +1,13 @@
+// http://boj.kr/0e7031ca17b447d78a5e4d26e349f773
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << fixed;
+  cout.precision(9);
+  int a, b;
+  cin >> a >> b;
+  cout << (double)a / b;
+}
