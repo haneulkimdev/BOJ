2ba1451b483e7d386d40a048518ce2d3d68be0be
Haneul Kim <haneul.kim.dev@gmail.com>
2023-01-16 19:43:52 +0900

    [BOJ] 15894번: 수학은 체육과목 입니다

---
+++ 15894.cpp
@@ -0,0 +1,11 @@
+// http://boj.kr/2ac78eb7279d4dbcae6ee99992a22fc2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  cout << 4LL * n;
+}
