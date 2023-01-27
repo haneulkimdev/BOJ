575498b61365d8b1ef901b2da6b80c468c1b9c74
Haneul Kim <haneul.kim.dev@gmail.com>
2023-01-27 16:19:32 +0900

    [BOJ] 6359번: 만취한 상범

---
+++ 6359.cpp
@@ -0,0 +1,15 @@
+// http://boj.kr/cbc162afb18b43c78c27a4b7e6ee3524
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << (int)sqrt(n) << '\n';
+  }
+}
