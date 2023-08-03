87f5413381aa4efa5b592c188f1d7aa0eb8e0b92
Haneul Kim <haneul.kim.dev@gmail.com>
2023-08-03 18:02:06 +0900

    [BOJ] 9372번: 상근이의 여행

---
+++ 9372.cpp
@@ -0,0 +1,19 @@
+// http://boj.kr/08cc6523326946958945d7210fca6512
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    while (m--) {
+      int a, b;
+      cin >> a >> b;
+    }
+    cout << n - 1 << '\n';
+  }
+}
